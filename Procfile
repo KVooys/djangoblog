436356acc536f3ab96252314269c6399ed87063e
Kurt Vooys <k.vooys@hotmail.com>
2019-06-19 18:44:05 +0200

    Added static file integration for Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn base_site.wsgi --log-file -
+web: python manage.py collectstatic; gunicorn base_site.wsgi --log-file -
--- base_site/settings.py
+++ base_site/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
